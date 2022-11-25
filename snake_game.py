2a4e43040f09765d05e89396fc0bfccab3ceb743
Rannael Santiago <74615002+rannaelsant@users.noreply.github.com>
2022-11-24 23:43:40 -0300

    Add files via upload

--- snake_game.py
+++ snake_game.py
@@ -1,14 +1,27 @@
+import time
 from turtle import Screen, Turtle
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game Python")
+screen.tracer(0)
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-for position in starting_positions:
-    segment_1 = Turtle(shape="square")
-    segment_1.color("white")
-    segment_1.goto(position)
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
 
 screen.exitonclick()
